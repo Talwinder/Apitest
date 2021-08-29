e9476eeb4d3fdcdc4e04f85d92d233733ff8d2c1
talsingh <talsingh@publicisgroupe.net>
2021-08-30 00:07:20 +0530

    test

--- TodoApi/Controllers/WeatherForecastController.cs
+++ TodoApi/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace TodoApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Frozen Tal", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Frozen Tal1", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
